5273576d8b8c488c4e43b652b81e52d336deb803
m11toh <martohmashah@gmail.com>
2024-05-23 12:53:43 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    str="Hello, programmer!"
+    print(str)
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
+
+
 
 def halve(number):
-    pass
+    return number / 2
